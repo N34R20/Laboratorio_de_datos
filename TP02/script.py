9a04139aba537c0d792f2b0420223cc5a800208c
NehuenYago <nehuenromero@protonmail.com>
2024-03-09 12:43:14 -0300

    corregi errores en asignacion de X e y, cambie el test size y agregue stratify

--- TP02/script.py
+++ TP02/script.py
@@ -252,9 +252,9 @@ print(df_ej_3.character.value_counts())
 #split dataset
 from sklearn.model_selection import train_test_split
 X = df_ej_3.iloc[:,2:]
-y = df_ej_2.iloc[:,1]
+y = df_ej_3.iloc[:, 0:1]
 
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33, random_state=42)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42, stratify=y['character'])
 
 #%%
 """
