b613cc396cc2e69dd5b37007ea9b9fbe68802bef
N34R20 <francisco.olmos.99@gmail.com>
2024-03-10 23:36:40 -0300

    push final

--- TP02/README.md
+++ TP02/README.md
@@ -1,1 +1,11 @@
 # TP 02
+
+El codigo se en encuentra en el archivo sign_DERecho_al_labo.py que esta modularizado para poder ejecitarse por partes en un ambiente de desarrollo spyder
+
+el ambiente puede ser reconstruido con el archivo requirements.txt con el administrador de paquetes conda
+
+- instructivo:
+
+```bash
+conda create --name nuevo_entorno --file requirements.txt
+```
--- TP02/sign_DERecho_al_labo.py
+++ TP02/sign_DERecho_al_labo.py
@@ -11,9 +11,16 @@ Created on Wed Feb 28 15:47:12 2024
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-#import seaborn as sns
+
+import random
+from sklearn.metrics import accuracy_score, recall_score, precision_score
+from sklearn.model_selection import train_test_split
+from sklearn.neighbors import KNeighborsClassifier
+
 import string
+
 #%%
+
 """
 Ejercicio 1
 Analisis Exploratorio de Datos
@@ -42,17 +49,9 @@ def map_labels_with_characters(characters:list, df:pd.DataFrame):
     return df
 
 df = map_labels_with_characters(characters=caracteres, df=df)
+
+print(df.character.value_counts())
 #%%
-
-print(df.head(10))
-print("\n")
-print(df.shape)
-
-"""
-Hay un total de 27455 imagenes y cada imagen esta compuesta por 784 pixeles -> 28^2 = 784
-"""
-#%%
-
 images, labels = df.iloc[:, 2:], df['character']
 
 # Displaying 16 random images
@@ -69,10 +68,15 @@ def displayImg(images, title):
 rand = np.random.randint(0, images.shape[0] - 16)
 displayImg(images.iloc[rand:rand + 16].values, labels.iloc[rand:rand + 16].values)
 
-
 #%%
 
-print(df.character.value_counts())
+print(df.head(10))
+print("\n")
+print(df.shape)
+
+"""
+Hay un total de 27455 imagenes y cada imagen esta compuesta por 784 pixeles -> 28^2 = 784
+"""
 
 #%%
 """
@@ -80,102 +84,93 @@ a. ¿Cuáles parecen ser atributos relevantes para predecir la letra a la que
 corresponde la seña? ¿Cuáles no? ¿Creen que se pueden descartar atributos?
 """
 
-promedios = np.zeros((24,784))
-X = [i for i in range(784)]
-fig, ax = plt.subplots()
-fila = 0
+desvios_por_pixel = df.std()
+X = [j for j in range(784)]
+fig,ax = plt.subplots()
+#for i in range(24):
+#    ax.plot(X,promedio_letra.iloc[i,1:])
+ax.plot(X,desvios_por_pixel.iloc[1:])
+ax.set_title('desvíos estándar de los valores por pixel')
+ax.set_xlabel('numero de pixel')
+ax.set_ylabel('desvio estandar (tonos de gris)')
 
-for num in range(26):
-    df_letra = df[df['label'] == num]
-    if df_letra.size > 0:
-
-#print(descripcion_letra_a.iloc[2,1:])
-        promedios_letra = df_letra.iloc[:,2:].mean()
-        promedios[fila,:] = promedios_letra
-        fila += 1
-    ax.plot(X, promedios_letra)
-
-#print(X)
-#%%
-
-desvios = np.zeros((24,784))
-X = [i for i in range(784)]
-fig, ax = plt.subplots()
-fila = 0
-
-for num in range(26):
-    df_letra = df[df['label'] == num]
-    if df_letra.size > 0:
-
-#print(descripcion_letra_a.iloc[2,1:])
-        desvios_letra = df_letra.iloc[:,2:].std()
-        desvios[fila,:] = desvios_letra
-        fila += 1
-    ax.plot(X, desvios_letra)
-    
-#%%
-desvios = []
-for i in range(784):
-    desvios.append(np.std(promedios[:,i]))
-print(desvios)
+"""
+Observamos mucha menor variabilidad en los primeros 100 píxeles que en el resto.
+De esto interpretamos que los primeros 100 píxeles podrían ser eliminados, mientras
+que los otros pueden ser más importantes para predecir la letra.
+"""
 
 #%%
-fig, ax = plt.subplots()
-ax.plot(X,desvios)
 
-#%%
 """
 b. ¿Hay señas que son parecidas entre sí? 
 Por ejemplo, ¿Qué es más fácil de diferenciar: 
 la seña de la E, de la seña de la L o la seña de la E de la seña de la M?
 """
 
-X = [i for i in range(784)]
-
 letra_E = df[df['character']=='e']
-#descripcion_letra_e = letra_E.describe()
-promedios_letra_e = letra_E.iloc[:,2:].mean() #tomamos la fila de promedios
+promedios_letra_e = letra_E.mean()
 
 letra_L = df[df['character']=='l']
-#descripcion_letra_l = letra_L.describe()
-promedios_letra_l = letra_L.iloc[:,2:].mean() #tomamos la fila de promedios
+promedios_letra_l = letra_L.mean()
 
 letra_M = df[df['character']=='m']
-#descripcion_letra_m = letra_M.describe()
-promedios_letra_m = letra_M.iloc[:,2:].mean() #tomamos la fila de promedios
-
-#fig,ax = plt.subplots()
-#ax.plot(X,promedios_letra_e)
-#ax.plot(X,promedios_letra_l)
-
-# la E y la L parecen tener señas similares, voy a comparar con la M
+promedios_letra_m = letra_M.mean()
 
 fig,ax = plt.subplots()
-ax.plot(X,promedios_letra_e)
-ax.plot(X,promedios_letra_m)
+ax.plot(X,promedios_letra_e[1:])
+ax.plot(X,promedios_letra_m[1:])
+ax.set_xlabel('pixel')
+ax.set_ylabel('color promedio')
+ax.set_title('tono por pixel promedio para las letras E y M')
 
-#print(promedios_letra_e)
-#print(promedios_letra_l)
+fig,ax = plt.subplots()
+ax.plot(X,promedios_letra_e[1:])
+ax.plot(X,promedios_letra_l[1:])
+ax.set_xlabel('pixel')
+ax.set_ylabel('color promedio')
+ax.set_title('tono por pixel promedio para las letras E y L')
 
+"""
+Como hay mucha más superposición entre los gráficos de las letras E y M,
+interpretamos que estas dos letras se parecen mucho más que la E y la L.
+"""
 
 #%%
+
 """
 c. Tomen una de las clases, por ejemplo la seña correspondiente a la C, 
 ¿Son todas las imágenes muy similares entre sí?
 """
 
+letra_C = df[df['character']=='c']
+letra_C.shape #hay 1144 filas
 
-c_sign = df[df['character'] == 'c']
-
-images, labels = c_sign.iloc[0:8, 2:], c_sign['character']
-
+images, labels = letra_C.iloc[0:8, 2:], letra_C['character']
 displayImg(images.values, labels.values)
+
+lista_filas = [i for i in range(1144)]
+random.seed(100)
+muestra = random.choices(lista_filas,k=5)
+print(muestra) #elegimos 10 números de fila aleatorios
+filas_al_azar_C = letra_C.iloc[muestra,2:]
+#filas_al_azar_C
+
+X = [j for j in range(784)]
+fig,ax = plt.subplots()
+for fila in range(5):
+    ax.plot(X,filas_al_azar_C.iloc[fila,:])
+ax.set_title('tonalidades por pixel para 5 letras C aleatorias')
+ax.set_xlabel('numero de pixel')
+ax.set_ylabel('tonalidad de gris')
 
 """
 Respuesta: En su mayoria si, cambia la forma de la mano en muchos casos, y la altura de la mano, 
 cosa que pueda confundir un algoritmo al buscar pixeles especificos 
 """
+
 #%%
+
 """
 d. Este dataset está compuesto por imágenes, 
 esto plantea una diferencia frente a los datos que utilizamos en las clases 
@@ -184,16 +179,18 @@ esto plantea una diferencia frente a los datos que utilizamos en las clases
 
 """
 Respuesta: Si, la complica, sobretodo porque los datos al ser no estructurados 
-no tienen atributos especificos, con lo cual las caracteristicas 
-son simplemente el gradiente de los pixeles individuales.
+no tienen atributos especificos,con lo cual las caracteristicas 
+son la tonalidad de los pixeles individuales. También hay muchos pixeles, lo que
+complica distinguir cuáles son más importantes, y existen
+imágenes con diferentes niveles de iluminación, con las manos de distintos tamaños
+y en diferentes ubicaciones, además de que a veces aparecen elementos aparte de la mano.
+Todo esto dificulta la identificación de la imagen con una letra.
 """
-
 #%%
 """
 Ejercicio 2
 Dada una imagen se desea responder la siguiente pregunta: 
 ¿la imagen corresponde a una seña de la L o a una seña de la A?
-
 """
 #%%
 """
@@ -204,90 +201,139 @@ de las letras L o A.
 df_ej_2 = df[(df['character'] == 'a') | (df['character'] == 'l')]
 
 #%%
-
 """
 b. Sobre este subconjunto de datos, analizar cuántas muestras se tienen
 y determinar si está balanceado con respecto a las dos clases a
 predecir (la seña es de la letra L o de la letra A).
 """
+# Printeamos las muestas de cada caracter y su proporcion sobre el total
+l_count = df_ej_2.character.value_counts()['l']
+a_count = df_ej_2.character.value_counts()['a']
+proporcion_l = l_count / (l_count + a_count)
+proporcion_a = a_count / (l_count + a_count)
+print("Cantidad de muestras 'L': ", l_count, " corresponde a ", "%.3f"%proporcion_l, " del dataset")
+print("Cantidad de muestras 'A': ", a_count, " corresponde a ", "%.3f"%proporcion_a, " del dataset")
 
-print(df_ej_2.character.value_counts())
+# Consideramos que esta balanceado ya que casi hay un 50% de muestras de cada clase
 
-"""
-Respuesta: .
-"""
+#%%
+
+letra_A = df[df['character']=='a']
+#descripcion_letra_l = letra_L.describe()
+promedios_letra_a = letra_A.iloc[:,2:].mean() #tomamos la fila de promedios
+
+letra_L = df[df['character']=='l']
+#descripcion_letra_m = letra_M.describe()
+promedios_letra_l = letra_L.iloc[:,2:].mean()
+
+fig,ax = plt.subplots()
+ax.plot(X,promedios_letra_a)
+ax.plot(X,promedios_letra_l)
+
+#%%
+images_A, labels_A = letra_A.iloc[0:8, 2:], letra_A['character']
+
+displayImg(images_A.values, labels_A.values)
+
+#%%
+images_L, labels_L = letra_L.iloc[0:8, 2:], letra_L['character']
+
+displayImg(images_L.values, labels_L.values)
 
 #%%
 """
 c. Separar os datos en conjuntos de train y test.
 """
-#split dataset
-from sklearn.model_selection import train_test_split
 
 X = df_ej_2.iloc[:,2:]
-y = df_ej_2.iloc[:,0]
+y = df_ej_2.iloc[:, 0:1]
 
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33, random_state=42)
-
-#%%
-from sklearn.neighbors import KNeighborsClassifier
-from sklearn.model_selection import GridSearchCV
-from sklearn.metrics import accuracy_score, recall_score, precision_score
-
-
-train_score = {}
-test_score = {}
-n_neighbors = np.arange(1, 30, 1)
-for neighbor in n_neighbors:
-    knn = KNeighborsClassifier(n_neighbors=neighbor)
-    knn.fit(X_train, y_train)
-
-    train_score[neighbor]=recall_score(y_train, knn.predict(X_train))
-    test_score[neighbor]=recall_score(y_test, knn.predict(X_test))
-
-#%%
-
-print(y)
-#print(X)
-#%%
-
-plt.plot(n_neighbors, train_score.values(), label="Train Accuracy")
-plt.plot(n_neighbors, test_score.values(), label="Test Accuracy")
-plt.xlabel("Numero de K")
-plt.ylabel("Accuracy")
-plt.title("KNN: Variando el numero de k's")
-plt.legend()
-#plt.xlim(0, 33)
-#plt.ylim(0.60, 0.90)
-plt.grid()
-plt.show()
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=8)
 
 #%%
 """
 d. Ajustar un modelo de KNN considerando pocos atributos, por ejemplo 3. 
-Probar con distintos conjuntos de 3 atributos y comparar resultados. Analizar utilizando otras cantidades de atributos.
-Importante: Para evaluar los resultados de cada modelo usar el conjunto de test generado en el punto anterior.
-OBS: Utilicen métricas para problemas de clasificación como por
-ejemplo, exactitud.
+Probar con distintos conjuntos de 3 atributos y comparar resultados.
+Analizar utilizando otras cantidades de atributos.
 """
+# Creamos el modelo de KNN con k = 3
+clf = KNeighborsClassifier(n_neighbors = 3)
 
-y_pred = clf.predict(X_test)
+resultados = []
+modelo = {}
 
-#accuracy = accuracy_score(y_test, y_pred)
-#precision = precision_score(y_test, y_pred)
-#recall = recall_score(y_test, y_pred, )
-                            
-#print(f"La accuracy en el conjunto de test para el modelo KNN es: {accuracy} \n \
-#      La precision en el conjunto de test para el modelo KNN es: {precision} \n \
-#      La recall en el conjunto de test para el modelo KNN es: {recall} \n")
+for i in range(10):
+    lista_numero_random = random.sample(range(1, 785), 3)
+    
+    atributo1 = 'pixel' + str(lista_numero_random[0])
+    atributo2 = 'pixel' + str(lista_numero_random[1])
+    atributo3 = 'pixel' + str(lista_numero_random[2])
+    
+    modelo['Atributo1'] = atributo1
+    modelo['Atributo2'] = atributo2
+    modelo['Atributo3'] = atributo3
+    
+    X_train_subset = X_train[[atributo1, atributo2, atributo3]]
+    
+    clf.fit(X_train, y_train.values.ravel())
+    clf.predict(X_test)
+    
+    modelo['Precision_score'] = precision_score(y_test, clf.predict(X_test),average='weighted')
+    modelo['Recall_score'] = recall_score(y_test, clf.predict(X_test),average='weighted')
+    modelo['R^2_score'] = clf.score(X_test, y_test)
+    
+    resultados.append(modelo)
+
+#%%
+print(resultados)
+
+
 #%%
 """
-e. Comparar modelos de KNN utilizando distintos atributos y distintos valores de k (vecinos). 
-Para el análisis de los resultados,tener en cuenta las medidas de evaluación (por ejemplo, la exactitud) 
-y la cantidad de atributos.
+e. Comparar modelos de KNN utilizando distintos atributos y distintos
+valores de k (vecinos). Para el análisis de los resultados, tener en
+cuenta las medidas de evaluación (por ejemplo, la exactitud) y la
+cantidad de atributos.
 """
 
+n_atributos = np.arange(1, 5, 1)
 
+n_neighbors = np.arange(1, 5, 1)
+
+train_score = {}
+test_score = {}
+
+for atributo in n_atributos:
+    train_score[atributo] = {}
+    test_score[atributo] = {}
+    
+    for neighbor in n_neighbors:
+
+        
+        X_train_subset = X_train.iloc[:, :atributo]
+        X_test_subset = X_test.iloc[:, :atributo]
+        
+        knn = KNeighborsClassifier(n_neighbors=neighbor)
+        knn.fit(X_train_subset, y_train)
+    
+        train_score[atributo][neighbor]=accuracy_score(y_train, knn.predict(X_train_subset))
+        test_score[atributo][neighbor]=accuracy_score(y_test, knn.predict(X_test_subset))
+
+
+#%%
+fig, axs = plt.subplots(2, 2, figsize=(10, 8))
+
+for i, ax in enumerate(axs.flat):
+    ax.plot(n_neighbors, train_score[i+1].values(), label="Train Accuracy")
+    ax.plot(n_neighbors, test_score[i+1].values(), label="Test Accuracy")
+    ax.set_xlabel("Numero de K")
+    ax.set_ylabel("Accuracy")
+    ax.set_title(f"Accuracy variando los k, usando {i+1} pixeles")
+    ax.legend()
+    ax.grid()
+
+plt.tight_layout()
+plt.show()
 
 #%%
 """
@@ -311,6 +357,7 @@ df_ej_3 = df[  (df['character'] == 'a')
              | (df['character'] == 'u')]
 
 print(df_ej_3.character.value_counts())
+
 #sSplit del dataset
 from sklearn.model_selection import train_test_split
 X = df_ej_3.iloc[:,2:]
@@ -428,22 +475,35 @@ arbol_final_modelo = DecisionTreeClassifier(criterion = mejores_parametros['crit
 # Entrenamos el modelo final con TODO el dataset. Estimamos que su performance es igual
 # o ligeramente mejor por haberlo entrenado con un dataset ligeramente mas grande
 arbol_final = arbol_final_modelo.fit(X, y)
-
+print(arbol_final)
 #%%
 # Visualizar el árbol de decisiones
-plt.figure(figsize=(12, 8))
+plt.figure(figsize=(30, 20))
 plot_tree(arbol_final, feature_names = X.columns, filled=True, fontsize=10)
 fig = plt.figure(figsize=(15,7))
 plt.show()
 #%%
 # Visualizar la importancia de las características
 importances = arbol_final.feature_importances_
-indices = sorted(range(len(importances)), key=lambda i: importances[i], reverse=True)
-print(importances)
-print(indices)
+importance_dict = dict()
+
+for i,v in enumerate(importances):
+ print('Feature: %0d, Score: %.5f' % (i,v))
+ importance_dict[i] = v
+
+num_zeros = sum(1 for valor in importance_dict.values() if valor == 0)
+
+print(f"Número de valores cero en el diccionario de importancias: {num_zeros}, de un total de {len(importance_dict)}")
+
+print(f"Eso quiere decir que el mejor modelo segun el K-Fold no considera relevante el {(num_zeros/len(importance_dict))*100:.2f}% de la features")
+# plot feature importance
+plt.title("Barplot sobre la importancia de Features segun el DecisionTree")
+plt.bar([x for x in range(len(importances))], importances)
+plt.xlabel("Pixeles")
+plt.ylabel("Importancia %")
+plt.show()
+
 #%%
-
-
 
 
 
